0c55d19da61d11dd2e5d8c88f71d1a0f00259643
jonallenaz <jonallenaz@gmail.com>
2013-07-31 16:02:06 -0700

    start session before setting cookie

--- timer/index.php
+++ timer/index.php
@@ -1,6 +1,6 @@
 <?php
-session_set_cookie_params(172800,"/timer/");
 session_start();
+session_set_cookie_params(172800,"/timer/");
 
 if(isset($_SESSION['t_username'])){
 		include('_in/index.php');
